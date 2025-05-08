e7dc20725ea830f3f8711040bb73b2cfdd8a3b67
Elaine Chao <arfydogs7@gmail.com>
2025-05-07 23:57:01 -0700

    skipped unfinished tests

--- __tests__/lab7.test.js
+++ __tests__/lab7.test.js
@@ -19,7 +19,9 @@ describe('Basic user flow for Website', () => {
   });
 
   // Check to make sure that all 20 <product-item> elements have data in them
-  it('Make sure <product-item> elements are populated', async () => {
+  // We use .skip() here because this test has a TODO that has not been completed yet.
+  // Make sure to remove the .skip after you finish the TODO. 
+  it.skip('Make sure <product-item> elements are populated', async () => {
     console.log('Checking to make sure <product-item> elements are populated...');
 
     // Start as true, if any don't have data, swap to false
@@ -48,6 +50,7 @@ describe('Basic user flow for Website', () => {
     **** TODO - STEP 1 ****
     * Right now this function is only checking the first <product-item> it found, make it so that
       it checks every <product-item> it found
+    * Remove the .skip from this it once you are finished writing this test.
     */
 
   }, 10000);
@@ -66,7 +69,7 @@ describe('Basic user flow for Website', () => {
 
   // Check to make sure that when you click "Add to Cart" on the first <product-item> that
   // the button swaps to "Remove from Cart"
-  it('Clicking the "Add to Cart" button should change button text', async () => {
+  it.skip('Clicking the "Add to Cart" button should change button text', async () => {
     console.log('Checking the "Add to Cart" button...');
 
     /**
@@ -75,13 +78,14 @@ describe('Basic user flow for Website', () => {
      * Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
      * Once you have the button, you can click it and check the innerText property of the button.
      * Once you have the innerText property, use innerText.jsonValue() to get the text value of it
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
   // number in the top right has been correctly updated
-  it('Checking number of items in cart on screen', async () => {
+  it.skip('Checking number of items in cart on screen', async () => {
     console.log('Checking number of items in cart on screen...');
 
     /**
@@ -89,12 +93,13 @@ describe('Basic user flow for Website', () => {
      * Query select all of the <product-item> elements, then for every single product element
        get the shadowRoot and query select the button inside, and click on it.
      * Check to see if the innerText of #cart-count is 20
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
   }, 10000);
 
   // Check to make sure that after you reload the page it remembers all of the items in your cart
-  it('Checking number of items in cart on screen after reload', async () => {
+  it.skip('Checking number of items in cart on screen after reload', async () => {
     console.log('Checking number of items in cart on screen after reload...');
 
     /**
@@ -102,37 +107,40 @@ describe('Basic user flow for Website', () => {
      * Reload the page, then select all of the <product-item> elements, and check every
        element to make sure that all of their buttons say "Remove from Cart".
      * Also check to make sure that #cart-count is still 20
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
   }, 10000);
 
   // Check to make sure that the cart in localStorage is what you expect
-  it('Checking the localStorage to make sure cart is correct', async () => {
+  it.skip('Checking the localStorage to make sure cart is correct', async () => {
 
     /**
      **** TODO - STEP 5 **** 
      * At this point the item 'cart' in localStorage should be 
        '[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]', check to make sure it is
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
   });
 
   // Checking to make sure that if you remove all of the items from the cart that the cart
   // number in the top right of the screen is 0
-  it('Checking number of items in cart on screen after removing from cart', async () => {
+  it.skip('Checking number of items in cart on screen after removing from cart', async () => {
     console.log('Checking number of items in cart on screen...');
 
     /**
      **** TODO - STEP 6 **** 
      * Go through and click "Remove from Cart" on every single <product-item>, just like above.
      * Once you have, check to make sure that #cart-count is now 0
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
   }, 10000);
 
   // Checking to make sure that it remembers us removing everything from the cart
   // after we refresh the page
-  it('Checking number of items in cart on screen after reload', async () => {
+  it.skip('Checking number of items in cart on screen after reload', async () => {
     console.log('Checking number of items in cart on screen after reload...');
 
     /**
@@ -140,19 +148,21 @@ describe('Basic user flow for Website', () => {
      * Reload the page once more, then go through each <product-item> to make sure that it has remembered nothing
        is in the cart - do this by checking the text on the buttons so that they should say "Add to Cart".
      * Also check to make sure that #cart-count is still 0
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
 
   }, 10000);
 
   // Checking to make sure that localStorage for the cart is as we'd expect for the
   // cart being empty
-  it('Checking the localStorage to make sure cart is correct', async () => {
+  it.skip('Checking the localStorage to make sure cart is correct', async () => {
     console.log('Checking the localStorage...');
 
     /**
      **** TODO - STEP 8 **** 
      * At this point he item 'cart' in localStorage should be '[]', check to make sure it is
+     * Remember to remove the .skip from this it once you are finished writing this test.
      */
-    
+
   });
 });
